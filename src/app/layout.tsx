c266aa24147b36e78f8b4a7c9591fbf6dd0c964a
ashwaniverma-github <ap8606574@gmail.com>
2025-09-21 08:42:24 +0530

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,16 @@ export const metadata: Metadata = {
   verification: {
     google: 'yiYpoh2nQF0BXHgGHh-CdOyIivMuK4kZtHZn7hf0TDs'
   },
+  icons: {
+    icon: [
+      { url: '/favicon.ico' },
+      // { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      // { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' }
+    ],
+    // apple: [
+    //   { url: '/apple-touch-icon.png' }
+    // ]
+  },
   openGraph: {
     type: "website",
     locale: "en_US",
